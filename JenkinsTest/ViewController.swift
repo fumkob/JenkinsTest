de8db4e78635551b8b934a8c2235128eee8e4cec
Fumiaki Kobayashi <fumiaki-kobayashi@mail.nissan.co.jp>
2020-06-30 17:41:16 +0900

    Initial Commit

---
+++ JenkinsTest/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  JenkinsTest
+//
+//  Created by Fumiaki Kobayashi on 2020/06/30.
+//  Copyright © 2020 Fumiaki Kobayashi. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
